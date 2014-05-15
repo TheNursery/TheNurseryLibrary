5de1ddc00262f2559aa444ebc00e7bbd25022f89
Stephanie J. Erickson <sje1g10@eve.(none)>
2014-05-15 18:33:47 +0100

    Moved morse production into one file (both direct audio output and wav files). Cleaner implementation.

--- soundproject/morse.py
+++ soundproject/morse.py
@@ -3,112 +3,198 @@ import random
 import struct
 import math
 import argparse
+import alsaaudio
 
-DOT_LENGTH = 10000
-DASH_LENGTH = 3*DOT_LENGTH
+class Morse:
 
-MORSE_DICT = {
-    'A': '.-',     'B': '-...',   'C': '-.-.', 
-    'D': '-..',    'E': '.',      'F': '..-.',
-    'G': '--.',    'H': '....',   'I': '..',
-    'J': '.---',   'K': '-.-',    'L': '.-..',
-    'M': '--',     'N': '-.',     'O': '---',
-    'P': '.--.',   'Q': '--.-',   'R': '.-.',
-    'S': '...',    'T': '-',      'U': '..-',
-    'V': '...-',   'W': '.--',    'X': '-..-',
-    'Y': '-.--',   'Z': '--..',
+    MORSE_DICT = {
+        'A': '.-',     'B': '-...',   'C': '-.-.', 
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+        'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+        
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.'
+        }
+
+    def __init__(self, dot_length, frequency, framerate, amplitude):
+        self.dot_length = dot_length
+        self.dash_length = 3*self.dot_length
+        self.lspace_len = self.dash_length
+        self.wspace_len = 2*self.dash_length
+
+        self.freq = frequency
+        self.framerate = framerate
+
+        if amplitude > 1.0: amplitude = 1.0
+        if amplitude < 0.0: amplitude = 0.0
+        amplitude = amplitude * 32767
+        self.amp = amplitude
+
+
+    def dot(self):
+        int_str_dot, pac_str_dot = self.tone(self.dot_length)
+        int_str_space, pac_str_space = self.space(self.dot_length)
+
+        return int_str_dot + int_str_space, pac_str_dot + pac_str_space
+
+    def dash(self):
+        int_str_dash, pac_str_dash = self.tone(self.dash_length)
+        int_str_space, pac_str_space = self.space(self.dot_length)
+
+        return int_str_dash + int_str_space, pac_str_dash + pac_str_space
+
+    def lspace(self):
+        return self.space(self.lspace_len)
+
+    def wspace(self):
+        return self.space(self.wspace_len)
+
+    def tone(self, length):
+
+        period = int(self.framerate / self.freq)
+        
+        length = length - length%period
+        
+        int_vals = []
+        pac_vals = []
+
+        for i in range(length):
+            val = int(float(self.amp) * 
+                      math.sin(2.0*math.pi*float(self.freq)*
+                               (float(i%period)/float(self.framerate))))
+            pv = struct.pack('h', val)
+            pac_vals.append(pv)
+            int_vals.append(str(val))
+
+        int_valstr = ''.join(int_vals)
+        pac_valstr = ''.join(pac_vals)
+        return int_valstr, pac_valstr
+
+    def space(self, length):
+
+        int_vals = []
+        pac_vals = []
+
+        for i in range(length):
+            pv = struct.pack('h', 0)
+            pac_vals.append(pv)
+            int_vals.append(str(0))
+
+        int_valstr = ''.join(int_vals)
+        pac_valstr = ''.join(pac_vals)
+        return int_valstr, pac_valstr
+
+    def getMorse(self, output_string, debug=False):
+
+        values = []
     
-    '0': '-----',  '1': '.----',  '2': '..---',
-    '3': '...--',  '4': '....-',  '5': '.....',
-    '6': '-....',  '7': '--...',  '8': '---..',
-    '9': '----.'
-    }
+        output_string = output_string.upper()
 
-def dot():
-    return tone(DOT_LENGTH) + space(DOT_LENGTH)
+        for char in output_string:
+            if char.isspace():
+                i, packed_value = self.wspace()
+                values.append(packed_value)
+                if (debug): print char
+            else:
+                morse_str = self.MORSE_DICT[char];
+                for dd in morse_str:
+                    if dd == '-':
+                        i, packed_value = self.dash()
+                    elif dd == '.':
+                        i, packed_value = self.dot()
+                    else:
+                        print 'character not understood:', dd
+                        break
+                    values.append(packed_value)
+                i, packed_value = self.lspace()
+                values.append(packed_value)
+                if (debug): print char, morse_str
 
-def dash():
-    return tone(DASH_LENGTH) + space(DOT_LENGTH)
+        value_str = ''.join(values)
 
-def lspace():
-    return space(DASH_LENGTH)
+        return value_str
 
-def wspace():
-    return space(2*DASH_LENGTH)
 
-def tone(length, frequency=900.0, framerate=44100, amplitude=0.5):
+def writeMorse(output_string, debug=False, filename='morse_output.wav',
+               frequency=450.0, dot_length=1000):
 
-    period = int(framerate / frequency)
-    if amplitude > 1.0: amplitude = 1.0
-    if amplitude < 0.0: amplitude = 0.0
-
-    amplitude = amplitude * 32767
-
-    vals = []
-    for i in range(length):
-        val = int(float(amplitude) * math.sin(2.0*math.pi*float(frequency)*(float(i%period)/float(framerate))))
-        pv = struct.pack('h', val)
-        vals.append(pv)
-
-    valstr = ''.join(vals)
-    return valstr
-
-def space(length):
-
-    vals = []
-    for i in range(length):
-        pv = struct.pack('h', 0)
-        vals.append(pv)
-
-    valstr = ''.join(vals)
-    return valstr
-
-def writeMorse(output_string, debug=False, filename='morse_output.wav'):
+    Fs = 44100
+    amp = 0.5
 
     noise_output = wave.open(filename, 'w')
-    noise_output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
+    noise_output.setparams((2, 2, Fs, 0, 'NONE', 'not compressed'))
 
-    values = []
-    
-    output_string = output_string.upper()
+    my_morse = Morse(dot_length, frequency, Fs, amp)
 
-    for char in output_string:
-        if char.isspace():
-            packed_value = wspace()
-            values.append(packed_value)
-            if (debug): print char
-        else:
-            morse_str = MORSE_DICT[char];
-            for dd in morse_str:
-                if dd == '-':
-                    packed_value = dash()
-                elif dd == '.':
-                    packed_value = dot()
-                else:
-                    print 'character not understood:', dd
-                    break
-                values.append(packed_value)
-            packed_value = lspace()
-            values.append(packed_value)
-            if (debug): print char, morse_str
+    value_str = my_morse.getMorse(output_string, debug=debug)
 
-    value_str = ''.join(values)
     noise_output.writeframes(value_str)
 
     noise_output.close()
 
     return filename
 
+def playMorse(output_string, debug=False, frequency=450.0, dot_length=1000):
+
+    channels = 2
+    sample_size = 2                     # bytes per sample
+    frame_size = channels * sample_size # bytes per frame
+    frame_rate = 44100                   # frames per second
+    byte_rate = frame_rate * frame_size # bytes per second
+    # 1 second worth of data per pcm.write() call
+    # decrease if shorter notes are needed
+    period_size = frame_rate
+
+    pcm = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK)
+    pcm.setchannels(channels)
+    pcm.setformat(alsaaudio.PCM_FORMAT_U8)
+    pcm.setrate(frame_rate)
+    pcm.setperiodsize(period_size)
+
+    amp = 0.5
+    my_morse = Morse(dot_length, frequency, frame_rate, amp)
+
+    valstr = my_morse.getMorse(output_string, debug=debug)
+
+    pcm.write(valstr)
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--debug', help="Run with Debug Output", default=0, type=int)
-    parser.add_argument('-f', '--filename', help="Filename to write wav file", default='morse_output.wav', type=str)
+    parser.add_argument('-d', '--debug', 
+                        help="Run with Debug Output", default=0, type=int)
+    parser.add_argument('-w', '--wavfile', 
+                        help="Output as wav file", default=0, type=int)
+    parser.add_argument('-f', '--filename', 
+                        help="Filename to write wav file", 
+                        default='morse_output.wav', type=str)
+    parser.add_argument('-l', '--dot_length', 
+                        help="Length of one dot", default=1000, type=int)
+    parser.add_argument('-F', '--frequency', 
+                        help="Carrier frequency", default=450.0, type=float)
     parser.add_argument('output_string', help="String to convert to morse")
     args = parser.parse_args()
 
-    fn = writeMorse(args.output_string, debug=args.debug, filename=args.filename)
+    if (args.wavfile):
+        fn = writeMorse(args.output_string, 
+                        debug=args.debug, 
+                        filename=args.filename,
+                        frequency=args.frequency,
+                        dot_length=args.dot_length)
+        print 'Wrote file to ', fn
+    else:
+        playMorse(args.output_string,
+                  debug=args.debug,
+                  frequency=args.frequency,
+                  dot_length=args.dot_length)
 
-    print 'Wrote file to ', fn
 
 if __name__ == "__main__":
     main()
